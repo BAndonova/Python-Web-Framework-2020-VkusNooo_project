060e6603db99b76edc9f5b159396f58d6e9b89d0
Bilyana Andonova <60629112+BAndonova@users.noreply.github.com>
2020-12-10 00:16:47 +0100

    ....

--- VkusNooo_project/settings.py
+++ VkusNooo_project/settings.py
@@ -101,6 +101,7 @@ WSGI_APPLICATION = 'VkusNooo_project.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),

667cc7204e655ac841e9751a9148deed5b5c15d1
Bilyana Andonova <60629112+BAndonova@users.noreply.github.com>
2020-12-09 23:23:27 +0100

    django_heroku settings

--- VkusNooo_project/settings.py
+++ VkusNooo_project/settings.py
@@ -14,6 +14,8 @@ from os.path import join
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Simplified static file serving.
@@ -154,3 +156,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = join(BASE_DIR, 'media')
 
 LOGIN_URL = '/auth/login/'
+
+django_heroku.settings(locals())

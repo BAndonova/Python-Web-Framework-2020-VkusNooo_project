dcbdfbf969f70a93c2e4caba8d2af3dddd047d8d
Bilyana Andonova <60629112+BAndonova@users.noreply.github.com>
2020-12-10 00:11:28 +0100

    ...

--- VkusNooo_project/settings.py
+++ VkusNooo_project/settings.py
@@ -14,6 +14,8 @@ from os.path import join
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Simplified static file serving.

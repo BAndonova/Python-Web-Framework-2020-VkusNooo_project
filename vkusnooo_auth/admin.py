a24c57577988d01be40acc4b4e31756770d69d6f
Bilyana Andonova <60629112+BAndonova@users.noreply.github.com>
2020-12-10 22:23:07 +0100

    admin login

--- vkusnooo_auth/admin.py
+++ vkusnooo_auth/admin.py
@@ -3,3 +3,14 @@ from django.contrib import admin
 from vkusnooo_auth.models import UserProfile
 
 admin.site.register(UserProfile)
+
+
+# class UserInLine(admin.TabularInline):
+#     model = UserProfile
+#
+# class UserProfileAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'profile_image', 'username', 'date_of_birth', 'creation_date')
+#     list_filter = ('user', 'profile_image', 'username', 'date_of_birth', 'creation_date')
+#     # inlines = (
+#     #     UserInLine,
+#     # )
